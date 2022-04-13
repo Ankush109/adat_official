b36e458b6f851015f57ecf6fe5f1d31038c4a255
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2022-04-13 23:49:00 +0530

    changes are being made in the ui

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -3,35 +3,16 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <Link class="navbar-brand" to="/">ADAT</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/products">Products</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/login">Login</a>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link" to="/account">Account</Link>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href='/cart'>cart</a>
-        </li>
-      </ul>
-    </div>
+    <div class="topnav">
+
+    <a class="active" href="/">Home</a>
+    <a href="/login">Login</a>
+    <a href="/products">Products</a>
+    <a href="/orders">Orders</a>
+    <a href="/cart">Cart</a>
+     
+
   </div>
-</nav>
-    </div>
   )
 }
 
